b7e2ceb5758df2108b3a3df8bb43f42e12c77453
柏喜 覃 <1377519410@qq.com>
2020-04-03 16:26:01 +0800

    新增品牌功能实现，使用qs工具解决参数传递问题

--- leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/BrandController.java
+++ leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/BrandController.java
@@ -4,12 +4,16 @@ import com.leyou.common.pojo.PageResult;
 import com.leyou.item.pojo.Brand;
 import com.leyou.service.IBrandService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("brand")
@@ -39,5 +43,17 @@ public class BrandController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(result);
+    }
+
+
+    /**
+     * 新增品牌
+     * @param brand
+     * @param cids
+     */
+    @PostMapping
+    public ResponseEntity<Void> saveBrand(Brand brand, @RequestParam("cids") List<Long> cids){
+        this.brandService.saveBrand(brand, cids);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
--- leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/service/IBrandService.java
+++ leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/service/IBrandService.java
@@ -3,6 +3,8 @@ package com.leyou.service;
 import com.leyou.common.pojo.PageResult;
 import com.leyou.item.pojo.Brand;
 
+import java.util.List;
+
 public interface IBrandService {
     /**
      * 根据查询条件分页并排序查询品牌信息
@@ -14,4 +16,11 @@ public interface IBrandService {
      * @return
      */
     PageResult<Brand> queryBrandsByPage(String key, Integer page, Integer rows, String sortBy, Boolean desc);
+
+    /**
+     * 新增品牌并插入中间表
+     * @param brand
+     * @param cids
+     */
+    void saveBrand(Brand brand, List<Long> cids);
 }
